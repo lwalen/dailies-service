e7d027531e9fe6bd33a10d780d51ae42df913587
Lars Walen <lars@walen.me>
2015-02-28 15:39:27 -0800

    Added some handy methods for completions

--- app/models/completion.rb
+++ app/models/completion.rb
@@ -1,4 +1,16 @@
 class Completion < ActiveRecord::Base
 
   belongs_to :task
+
+  def complete?
+    completed_at.try(:to_date) == created_at.to_date
+  end
+
+  def complete!
+    self.update_attribute(:completed_at, Time.now)
+  end
+
+  def incomplete!
+    self.update_attribute(:completed_at, nil)
+  end
 end
